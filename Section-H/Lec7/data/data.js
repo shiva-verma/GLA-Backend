aaa61cc3d3738224d3d2c6d224efec52321c483f
shiva-verma <jztshiva@gmail.com>
2024-02-06 09:50:23 +0530

    project section-H

--- Section-H/Lec7/data/data.js
+++ Section-H/Lec7/data/data.js
@@ -1,24 +1,30 @@
 const user = [
     {
+        id:1,
         name:"rudra",
         age:15,
         city:"Mumbai"
     },
     {
+        id:2,
         name:"abcde",
         age:24,
         city:"delhi"
     },
     {
+        id:3,
         name:"Aman",
         age:23,
         city:"chennai"
     },
     {
+        id:4,
         name:"saurabh",
         age:20,
         city:"gujrat"
-    }, {
+    }, 
+    {
+        id:5,
         name:"Anmol",
         age:19,
         city:"Haryana"
--- Section-H/Lec7/index.js
+++ Section-H/Lec7/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const userData = require('./data/data.js')
 const app = express();
+const methodOverride = require('method-override');
+
 app.use(express.urlencoded({extended:true}))
+app.use(methodOverride('_method'));
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
@@ -15,15 +18,66 @@ app.get('/user/new',(req, res)=>{
     res.render("newUser")
 })
 
-app.post('/newUser', (req, res)=>{
+app.post('/users', (req, res)=>{
     const name = req.body.name;
     const age = req.body.age;
     const city = req.body.city;
-
+    // console.log(`name = ${name} && age = ${age} && city = ${city}`)
     const newUser = {
-        name,age, city
+        id:userData.length + 1,
+        name,age,city
     }
     userData.push(newUser);
+    res.redirect('/users')
+})
+
+app.get('/user/:id', (req, res)=>{
+
+    const id = req.params.id;
+
+    const user = userData.find((u) => u.id == id)
+
+    console.log(user);
+
+    res.render("singleUser", {user})
+})
+
+app.get('/user/:id/new', (req, res)=>{
+    const id = req.params.id;
+
+    const user = userData.find((u) => u.id == id)
+
+    res.render("EditUser", {user})
+
+})
+
+app.patch('/user/:id', (req, res)=>{
+    const id = req.params.id;
+
+    const user = userData.find((u)=> u.id == id);
+
+    const {name, age, city} = req.body;
+
+    user.name = name;
+    user.age= age;
+    user.city = city;
+
+    res.redirect('/users')
+})
+
+app.delete('/user/:id', (req, res)=>{
+    const id = req.params.id;
+
+    const user = userData.find((u)=> u.id == id);
+
+    // const index = userData.indexOf(user);
+
+    // userData.splice(index, 1);
+
+    const filterUser = userData.filter((u)=> u.id != id);
+    userData = filterUser;
+
+    res.redirect('/users');
 })
 
 app.listen(1212, ()=>{
