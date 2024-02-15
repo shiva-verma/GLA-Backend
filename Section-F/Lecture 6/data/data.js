1a4128c064e12a63b19be21ac60490d1e9414b5e
shiva-verma <jztshiva@gmail.com>
2024-02-15 14:27:40 +0530

    CRUD

--- Section-F/Lecture 6/data/data.js
+++ Section-F/Lecture 6/data/data.js
@@ -1,20 +1,24 @@
 const user = [
     {
+        id:1,
       name : "abcde",
       age: 24,
       city :"delhi"  
     },
     {
+        id:2,
         name : "anmol",
         age: 20,
         city :"mumbai"  
     },
     {
+        id:3,
         name : "shivam",
         age: 19,
         city :"banglore"  
     },
     {
+        id:4,
         name : "sachin",
         age: 23,
         city :"gujrat"  
--- Section-F/Lecture 6/index.js
+++ Section-F/Lecture 6/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
-const userData = require('./data/data.js')
+let userData = require('./data/data.js')
 const app = express();
+const fs = require('fs');
+const methodOverride = require('method-override')
+
+const location = path.join(__dirname, "data", "data.js")
 
 app.set('view engine', 'ejs');
 app.set('views', './views')
 
+app.use(methodOverride('_method'))
 app.use(express.urlencoded({extended:true}));
 
 app.get('/users', (req, res)=>{
@@ -20,11 +25,61 @@ app.post('/users', (req, res)=>{
     const {name, age, city} = req.body;
 
     const newUser = {
+        id: userData.length + 1,
         name, city, age
     }
     userData.push(newUser);
+    fs.writeFile(location, userData, (err)=>{
+
+    })
+
     res.redirect('/users')
 })
+
+app.get('/user/:id', (req, res)=>{
+    const id = req.params.id
+
+    const user = userData.find((u)=> u.id == id)
+
+    res.render("singleUser", {user});
+})
+
+app.get('/user/edit/:id',(req, res)=>{
+    const id = req.params.id
+
+    const user = userData.find((u)=> u.id == id)
+    res.render("editUser", {user});
+})
+
+app.patch('/user/:id', (req, res)=>{
+    const id = req.params.id
+
+    const user = userData.find((u)=> u.id == id)
+    const {name, age, city} = req.body;
+
+    user.name = name;
+    user.age = age;
+    user.city = city;
+
+    res.redirect('/users')
+})
+
+app.delete('/user/:id',(req, res)=>{
+    const id = req.params.id;
+
+    const user = userData.find((u)=> u.id == id);
+
+    // const index = userData.indexOf(user);
+
+    // userData.splice(index, 1);
+
+    const filterUser = userData.filter((u)=>  u.id != id)
+
+    userData = filterUser;
+
+    res.redirect('/users');
+})
+
 
 app.listen(3636,()=>{
     console.log(`server is running at port 3636`)
