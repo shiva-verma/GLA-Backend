eee4c815b9e1ea52ac94c6686373fd304c1c1651
shiva-verma <jztshiva@gmail.com>
2024-03-29 09:54:20 +0530

    29-3-24

--- Section-H/Major-Project/index.js
+++ Section-H/Major-Project/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dbConnect = require("./db/db.js");
 const app = express();
 const path = require("path");
-const Product = require("./model/productModel.js")
+const Product = require("./model/productModel.js");
 const port = 5000;
 
 dbConnect();
@@ -17,9 +17,22 @@ app.get("/product/new", (req, res) => {
   res.render("addProduct");
 });
 
+app.get("/product", async(req, res)=>{
+  const allProduct = await Product.find({})
+  // console.log(allProduct);
+  res.render("home", {allProduct});
+})
+
 app.post("/product",async(req, res)=>{
     await Product.create(req.body);
-    res.send("hi")
+    res.redirect('/product');
+})
+
+app.get("/product/:id", async(req, res)=>{
+    const id = req.params.id;
+    const singleProduct = await Product.findById(id);
+    console.log(singleProduct);
+    res.render("singleProduct", {singleProduct});
 })
 
 app.listen(port, () => {
