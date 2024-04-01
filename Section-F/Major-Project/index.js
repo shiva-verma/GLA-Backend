68a4bd00142997772c23b5801d493c3ac64ae684
shiva-verma <jztshiva@gmail.com>
2024-04-01 14:56:12 +0530

    1-1-24

--- Section-F/Major-Project/index.js
+++ Section-F/Major-Project/index.js
@@ -3,6 +3,7 @@ const dbConnect = require("./db/db.js");
 const path = require("path");
 const Product = require("./model/productModel.js");
 const app = express();
+const methodOverride = require('method-override')
 const port = 4000;
 
 dbConnect();
@@ -10,6 +11,7 @@ app.set("view engine", "ejs");
 app.set("views", "views")
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
 
 app.get("/product/new", (req, res) => {
   res.render("addProduct");
@@ -40,6 +42,28 @@ app.get("/product", async(req, res)=>{
   const allProduct = await Product.find({});
   // console.log(allProduct);
   res.render("home", {allProduct});
+})
+
+app.get('/product/:id', async(req, res)=>{
+  const {id} = req.params;
+  const product = await Product.findById(id);
+  res.render('singleProduct', {item:product});
+})
+
+app.get('/product/:id/edit', async(req, res)=>{
+    const {id} = req.params;
+    const product = await Product.findById(id);
+     res.render('edit', {i:product});
+})
+
+app.patch('/product/:id', async(req, res)=>{
+    const {id} = req.params;
+
+    const {productName, price, description, imageUrl} = req.body;
+
+    await Product.findByIdAndUpdate(id, {productName, price, description, imageUrl});
+
+    res.redirect(`/product/${id}`);
 })
 
 app.listen(port, () => {
