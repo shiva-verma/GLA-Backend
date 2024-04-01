05db4bac7a5d2e9fd285b5ed61a1f606b5829b0b
shiva-verma <jztshiva@gmail.com>
2024-04-01 11:55:04 +0530

    1-4-24

--- Section-E/MAJOR-PROJECT/index.js
+++ Section-E/MAJOR-PROJECT/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dbConnect = require("./db/db.js");
 const path = require("path");
 const Product = require("./model/productModel.js");
+const methodOverride = require('method-override');
 const app = express();
 
 app.use(express.json());
@@ -9,6 +10,7 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")))
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
 
 dbConnect();
 
@@ -33,6 +35,34 @@ app.get("/product/:id", async(req, res)=>{
     // console.log(id);
     const singleProduct = await Product.findById(id)
     res.render("singleProduct", {item:singleProduct})
+})
+
+app.get('/product/:id/edit', async(req, res)=>{
+  const {id} = req. params
+    const singleProduct = await Product.findById(id)
+    res.render('edit', {i:singleProduct})
+})
+
+app.patch('/product/:id',async(req, res)=>{
+    //find product with given id in params
+    const {id} = req.params;
+    const product = await Product.findById(id);
+
+    //get all new values from form
+    const {productName, price, description, imageUrl} = req.body;
+
+    // updated old values with new values
+    await Product.findByIdAndUpdate(id, {productName, price, description, imageUrl});
+
+    //redirect
+    res.redirect(`/product/${id}`);
+})
+
+app.delete('/product/:id', async(req, res)=>{
+  const {id} = req.params;
+
+  await Product.findByIdAndDelete(id);
+  res.redirect('/product');
 })
 
 const port = 5000;
