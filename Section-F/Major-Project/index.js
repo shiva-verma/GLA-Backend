b1c8e59b11bbf944834a5cf7ca264bb99c49a405
shiva-verma <jztshiva@gmail.com>
2024-04-03 15:46:18 +0530

    3-4-24

--- Section-F/Major-Project/index.js
+++ Section-F/Major-Project/index.js
@@ -3,68 +3,22 @@ const dbConnect = require("./db/db.js");
 const path = require("path");
 const Product = require("./model/productModel.js");
 const app = express();
-const methodOverride = require('method-override')
+const methodOverride = require('method-override');
+const ejsmate = require('ejs-mate');
+const productRouter = require('./routes/productRoutes.js');
 const port = 4000;
 
 dbConnect();
+app.engine('ejs', ejsmate);
 app.set("view engine", "ejs");
 app.set("views", "views")
+
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
+app.use(productRouter);
 
-app.get("/product/new", (req, res) => {
-  res.render("addProduct");
-});
 
-app.post("/product", async(req, res)=>{
-    // const productName = req.body.productName;
-    // const price = req.body.price;
-    // const description = req.body.description;
-    // const imageUrl = req.body.imageUrl;
-
-    // const newProduct = new Product({
-    //   productName,
-    //   price,
-    //   description,
-    //   imageUrl
-    // })
-
-    // await newProduct.save();
-
-    await Product.create(req.body);
-
-    // res.send("hiii");
-    res.redirect("/product");
-})
-
-app.get("/product", async(req, res)=>{
-  const allProduct = await Product.find({});
-  // console.log(allProduct);
-  res.render("home", {allProduct});
-})
-
-app.get('/product/:id', async(req, res)=>{
-  const {id} = req.params;
-  const product = await Product.findById(id);
-  res.render('singleProduct', {item:product});
-})
-
-app.get('/product/:id/edit', async(req, res)=>{
-    const {id} = req.params;
-    const product = await Product.findById(id);
-     res.render('edit', {i:product});
-})
-
-app.patch('/product/:id', async(req, res)=>{
-    const {id} = req.params;
-
-    const {productName, price, description, imageUrl} = req.body;
-
-    await Product.findByIdAndUpdate(id, {productName, price, description, imageUrl});
-
-    res.redirect(`/product/${id}`);
-})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port} 🔥`);
