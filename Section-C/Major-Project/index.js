bffaecc1da5a6f65f542443c935e4a09d1fd7d34
shiva-verma <jztshiva@gmail.com>
2024-04-04 13:53:34 +0530

    4-4-24

--- Section-C/Major-Project/index.js
+++ Section-C/Major-Project/index.js
@@ -4,64 +4,21 @@ const path = require("path");
 const Product = require("./model/productModel.js");
 const app = express();
 const methodOverride = require("method-override");
+const ejsmate = require('ejs-mate');
+const productRouter = require('./routes/productRoutes.js');
 const port = 4000;
 
 dbConnect();
+app.engine('ejs', ejsmate)
 app.set("view engine", "ejs");
 app.set("views", "views")
+
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
+app.use(productRouter);
 
-app.get("/product/new", (req, res) => {
-  res.render("addProduct");
-});
 
-app.post("/product", async(req, res)=>{
-  //  const productName = req.body.productName
-  //  const price = req.body.price
-  //  const description = req.body.description
-  //  const imageUrl = req.body.imageUrl
-
-  //  const newProduct = new Product({
-  //      productName,
-  //      price,
-  //      description,
-  //      imageUrl
-  //  })
-
-  // await newProduct.save();
-
-  await Product.create(req.body)
-  res.send("hiii")
-
-})
-
-app.get("/product", async(req, res)=>{
-  const allProduct = await Product.find({});
-  console.log(allProduct);
-  res.render("home", {allProduct});
-})
-
-app.get('/product/:id', async(req, res)=>{
-  const {id} = req.params
-  const singleProduct = await Product.findById(id);
-  res.render("singleProduct", {product:singleProduct});
-})
-
-app.get('/product/:id/edit', async(req, res)=>{
-  // find product with given id in the params
-  const {id} = req.params
-  const product = await Product.findById(id);
-
-  res.render('edit', {i:product});
-})
-
-app.patch('/product/:id', async(req, res)=>{
-  const {id} = req.params;
-  await Product.findByIdAndUpdate(id, );
-  res.redirect(`/product/${id}`);
-})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port} 🔥`);   
---
+++ Section-C/Major-Project/routes/productRoutes.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const router = express.Router();
+const Product = require('../model/productModel.js')
+
+router.get("/product/new", (req, res) => {
+    res.render("products/addProduct");
+  });
+  
+  router.post("/product", async(req, res)=>{
+    //  const productName = req.body.productName
+    //  const price = req.body.price
+    //  const description = req.body.description
+    //  const imageUrl = req.body.imageUrl
+  
+    //  const newProduct = new Product({
+    //      productName,
+    //      price,
+    //      description,
+    //      imageUrl
+    //  })
+  
+    // await newProduct.save();
+  
+    await Product.create(req.body)
+    res.send("hiii")
+  
+  })
+  
+  router.get("/product", async(req, res)=>{
+    const allProduct = await Product.find({});
+    console.log(allProduct);
+    res.render("products/home", {allProduct});
+  })
+  
+  router.get('/product/:id', async(req, res)=>{
+    const {id} = req.params
+    const singleProduct = await Product.findById(id);
+    res.render("products/singleProduct", {product:singleProduct});
+  })
+  
+  router.get('/product/:id/edit', async(req, res)=>{
+    // find product with given id in the params
+    const {id} = req.params
+    const product = await Product.findById(id);
+    
+  
+  
+    res.render('products/edit', {i:product});
+  })
+  
+  router.patch('/product/:id', async(req, res)=>{
+    const {id} = req.params;
+  
+    const {productName, price, description, imageUrl} = req.body
+  
+    await Product.findByIdAndUpdate(id, {productName, price, description, imageUrl});
+    res.redirect(`/product/${id}`);
+  })
+  
+  router.delete('/product/:id', async(req, res)=>{
+      const {id} = req.params;
+  
+      await Product.findByIdAndDelete(id);
+  
+      res.redirect('/product');
+  })
+
+module.exports = router;
