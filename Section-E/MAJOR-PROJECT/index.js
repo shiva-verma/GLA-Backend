19d0d21d773a1b868c2dbfb6584915c8abee7d8e
shiva-verma <jztshiva@gmail.com>
2024-04-15 11:57:27 +0530

    15--4-24

--- Section-E/MAJOR-PROJECT/index.js
+++ Section-E/MAJOR-PROJECT/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dbConnect = require("./db/db.js");
 const path = require("path");
 const Product = require("./model/productModel.js");
+const User = require('./model/userModel.js');
 const methodOverride = require('method-override');
 const ejsmate = require('ejs-mate');
 const productRouter = require('./routes/productRoutes.js');
@@ -9,6 +10,8 @@ const reviewRouter = require('./routes/reviewRoutes.js');
 const userRouter = require('./routes/userRoutes.js')
 const flash = require('connect-flash');
 const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
 const app = express();
 
 app.use(express.json());
@@ -20,12 +23,22 @@ app.set("view engine", "ejs");
 app.use(session({
     secret: 'keyboard cat',
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: true,
+    cookie:{
+        httpOnly:true,
+        maxAge: 10000
+    }
 }))
 
 app.use(express.static(path.join(__dirname, "public")))
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
+
+passport.use(new LocalStrategy(User.authenticate()));
+app.use(passport.authenticate("session"));
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 app.use(flash());
@@ -36,6 +49,7 @@ app.use((req, res, next)=>{
     res.locals.error = req.flash("error");
     res.locals.warning = req.flash("warning");
     res.locals.info = req.flash("info");
+    res.locals.currUser = req.user;
     next();
 })
 
--- Section-E/MAJOR-PROJECT/routes/userRoutes.js
+++ Section-E/MAJOR-PROJECT/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router();
 const User = require('../model/userModel');
+const passport = require('passport');
 
 router.get('/login', (req, res)=>{
     res.render('auth/login')
@@ -15,9 +16,18 @@ router.post('/register',async(req, res)=>{
     const {username, email, password} = req.body;
 
     const user = new User({username, email});
+    // await User.create({username, email, password});
 
     await User.register(user, password);
+
+    res.redirect('/login');
 })
+
+router.post('/login', 
+passport.authenticate('local', { failureRedirect: '/login' }),
+(req, res) => {
+  res.redirect('/product');
+});
 
 
 module.exports = router;
